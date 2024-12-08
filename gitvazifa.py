883d9bcbdbb3e7e85859a530197606ceb4a2731a
Mohinur-gith <ergashevam384@gmail.com>
2024-12-08 13:38:25 +0500

    2 likdan 16 likka o'tish dasturi

--- gitvazifa.py
+++ gitvazifa.py
@@ -1,6 +1,7 @@
 binary_number = input("Ikkilik sonni kiriting: ")
 try:
     decimal_number = int(binary_number, 2)
-    print(f"Ikkilik son {binary_number} o'nlikda {decimal_number}")
+    hexadecimal_number = hex(decimal_number)[2:].upper()
+    print(f"Ikkilik son {binary_number} o'nlikda {decimal_number} va o'n oltilikda {hexadecimal_number}")
 except ValueError:
     print("Iltimos, faqat 0 va 1 raqamlaridan tashkil topgan to'g'ri ikkilik son kiriting.")
--- vazifa1.py
+++
@@ -1,8 +0,0 @@
-# import math as m
-# x = float(input("x = "))
-# y = float(input("y = "))
-# z = float(input("z = "))
-# b = m.pow(y,m.pow(m.fabs(x),(1/3)))+m.pow(m.cos(y),3)*\
-#     abs(x-y)*(1+(m.pow(m.sin(z),2)/m.sqrt(x+y)))/(m.exp(m.fabs(x-y))+x/2)
-# print("b = ",b)
-
